21e6a883a7470888fc892bf4791c8a2bfe151082
minhao <zhangminhao12137@gmail.com>
2022-04-06 13:29:30 -0700

    finished hw2

--- hw2/task2.sh
+++ hw2/task2.sh
@@ -1,67 +1,52 @@
 #!/bin/bash
 
 ####################################
-# Name: <Minhao Zhang>
-# CSE 391 - Winter 2021 
-# Homework 1 - Task 4
+# Name: <Your name here>
+# CSE 391 - Spring 2022
+# Homework 2 - Task 2
 ####################################
 
 function problem1 {
   # Type your answer to problem #1 below this line
-  cp MyProgram.java ./java
+  javac ParseColumn.java
 }
 
 function problem2 {
   # Type your answer to problem #2 below this line
-  ls -l
+  java ParseColumn 1 < intro_survey.csv > candies.txt
 }
 
 function problem3 {
   # Type your answer to problem #3 below this line
-  ls -arl /var
+  grep chocolate < candies.txt -i
 }
 
 function problem4 {
   # Type your answer to problem #4 below this line
-  mv Burrot.java Borat.java
+  grep chocolate < candies.txt -iv
 }
 
 function problem5 {
   # Type your answer to problem #5 below this line
-  rm diff.html diff.css
+  tail -n +2 intro_survey.csv > intro_survey_no_header.csv
 }
 
 function problem6 {
   # Type your answer to problem #6 below this line
-  touch -mt 202001010415 MyProgram.java
+  wc intro_survey_no_header.csv -l
 }
 
 function problem7 {
   # Type your answer to problem #7 below this line
-  ls *.html *.css
+  java ParseColumn 2 < intro_survey.csv | tail -n 1
 }
 
 function problem8 {
   # Type your answer to problem #8 below this line
-  cp *.txt ./java
+  java ParseColumn 4 < intro_survey_no_header.csv | grep "no" -i| wc -l
 }
 
 function problem9 {
   # Type your answer to problem #9 below this line
-  diff lyrics.txt lyrics2.txt
-}
-
-function problem10 {
-  # Type your answer to problem #10 below this line
-  cat lyrics.txt
-}
-
-function problem11 {
-  # Type your answer to problem #11 below this line
-  cat song*.txt
-}
-
-function problem12 {
-  # Type your answer to problem #12 below this line
-  head -n 7 animals.txt
+  java ParseColumn 1 < intro_survey_no_header.csv | sort -f | uniq -i | wc -l
 }
