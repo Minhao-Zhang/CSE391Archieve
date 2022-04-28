38ebda58ab6d41561fc5d369439527ea0981ea82
Minhao Zhang <zmh5201@uw.edu>
2022-04-27 22:08:41 -0700

    finished hw5

---
+++ hw5/homework5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+####################################
+# Name: <Your Name Here>
+# CSE 391 - Spring 2022
+# Homework 5
+####################################
+
+function print_username {
+  echo "zmh5201"
+}
